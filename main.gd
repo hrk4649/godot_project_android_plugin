1064b09d7906f323ee27594d79deea889ed57742
Hiroki Taira <58840595+hrk4649@users.noreply.github.com>
2023-06-02 06:50:54 +0900

    initial files

---
+++ main.gd
@@ -0,0 +1,31 @@
+extends Control
+
+const plugin_name = "AndroidGodotPluginTemplate"
+
+@onready var label = get_node("%Label")
+
+var plugin
+
+func _ready():
+	if Engine.has_singleton(plugin_name):
+		plugin = Engine.get_singleton(plugin_name)
+		plugin.connect("wake_up", Callable(self, "_on_wake_up"))
+		plugin.connect("wake_up2", Callable(self, "_on_wake_up2"))
+		label.text = plugin.getHelloWorldNative()
+	else:
+		label.text = "Plugin not loaded."
+		print("Plugin not loaded.")
+
+func _on_wake_up():
+	label.text = "wake_up"
+
+func _on_wake_up2(time):
+	label.text = "wake_up at %s" % time
+
+func _on_button_pressed():
+	print("_on_button_pressed")
+	plugin.signalTest()
+
+func _on_button_2_pressed():
+	print("_on_button_2_pressed")
+	plugin.signalTest2()
